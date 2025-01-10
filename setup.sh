b9b66c5b6ae8ef7b256a062478a4ec3e76194b40
Jeremy C. Andrus <jchristian.andrus@gmail.com>
2025-01-10 01:10:43 -0800

    small setup script updates
    Signed-off-by: Jeremy C. Andrus <jchristian.andrus@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
-[ -d .venv ] && exit 0
+if [ -d .venv ]; then
+	echo "source .venv/bin/activate"
+	exit 0
+fi
 
 python3 -m venv .venv
-source ./venv/bin/activate
+source .venv/bin/activate
+chmod 755 .venv/bin/activate
 pip3 install httpx
 pip3 install dataclasses_json
