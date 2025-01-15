5d073385e40ea65d9a18bd0b6a977af24146b76e
Jeremy C. Andrus <jchristian.andrus@gmail.com>
2025-01-15 02:16:07 -0800

    split BigTableData building into separate script
    Signed-off-by: Jeremy C. Andrus <jchristian.andrus@gmail.com>

--- test/test-data.py
+++ test/test-data.py
@@ -55,46 +55,5 @@ def TestBasicDataAPIs():
 
     return sumodata
 
-def BuildBigTable(sumodata = None, preload_file = './sumodata_200001_201212.pickle'):
-    startDate = date(2000,1,1)
-    endDate = date(2024,11,30)
 
-    curDate = startDate # date(2013,1,1) # startDate
-
-    if not sumodata:
-        if os.path.isfile(preload_file):
-            print(f'Loading data from {preload_file}...')
-            try:
-                sumodata = SumoData.load_data(preload_file)
-            except:
-                sumodata = SumoData()
-                sys.stderr.write(f'Error loading data from {preload_file}...')
-                pass
-        else:
-            sumodata = SumoData()
-
-    sumodata.print_table_stats()
-
-    # Try to load the 201103 basho b/c it's all goofed up
-    print(f'Trying to update 201103...')
-    sumodata.add_basho_by_date_range(date(2011,2,1), date(2011,4,1))
-    sumodata.update_basho(date(2011,3,1))
-    sumodata.print_table_stats()
-
-    # run through the date range in year increments and save after each year
-    while curDate < endDate:
-        bStart = curDate
-        bEnd = curDate + relativedelta(years=1)
-        sumodata.add_basho_by_date_range(bStart, bEnd)
-        sumodata.print_table_stats()
-        # create new files after each year just to see the progress
-        fname = f'sumodata_{BashoIdStr(startDate)}_{BashoIdStr(bEnd)}.pickle'
-        print(f'Saving Data in {fname}')
-        sumodata.save_data(fname)
-        curDate = bEnd + relativedelta(months=1)
-
-    return
-
-#t = TestBasicDataAPIs()
-# BuildBigTable(t)
-BuildBigTable()
+t = TestBasicDataAPIs()
