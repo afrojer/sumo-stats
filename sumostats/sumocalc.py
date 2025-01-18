eb79da3fbfaeaa378442be8d3e72902334d9e515
Jeremy C. Andrus <jchristian.andrus@gmail.com>
2025-01-18 01:57:52 -0800

    sumocalc: interfaces/classes for prediction
    Signed-off-by: Jeremy C. Andrus <jchristian.andrus@gmail.com>

--- sumostats/sumocalc.py
+++ sumostats/sumocalc.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
-from .sumoapi import *
+from .sumodata import *
 
+import random
 from datetime import date
 from dateutil.relativedelta import *
 import sys
 
-class BoutCompare():
-    def __init__(self, data:SumoData):
+"""
+SumoBoutCompare
+
+Base class for generating a probability of the outcome of a bout. To create a comparator,
+derive a new class, and override the compare() method. In that method, you can use
+self._data as a SumoData object if you need additional context.
+
+"""
+class SumoBoutCompare():
+    def __init__(self, data:SumoData, weight:float = 1.0):
         self._data = data
+        self._weight = weight
+        if not self._data:
+            raise Exception('data cannot be empty/None')
 
     # Override this method
     #
@@ -19,13 +31,48 @@ class BoutCompare():
     def compare(self, matchup:SumoMatchup):
         pass
 
-    def __call__(self, matchup:SumoMatchup):
-        return self.compare(matchup)
+    def weight(self):
+        return self._weight
 
-#
-# Compare Rikishi based on BMI
-#
-class CompareBMI(BoutCompare):
-    def compare(self, matchup):
-        return 1.0 - matchup.rikishi.bmi() / matchup.opponent.bmi()
+    def __call__(self, matchup:SumoMatchup):
+        return self.compare(matchup) * self._weight
+
+"""
+BoutPredictor
+
+This class holds a list of comparisons, each of which generate a probability of bout
+outcome. The predict() function takes a SumoMatchup object and returns a predicted
+Rikishi winner along with a confidence (float).
+
+"""
+class SumoBoutPredictor():
+    def __init__(self, data:SumoData, comparison:list[SumoBoutCompare] = []):
+        self._data = data
+        self._comparison = comparison
+
+    def add_comparison(self, compare:SumoBoutCompare):
+        self._comparison.append(compare)
+        return
+
+    def add_comparisons(self, compare:list[SumoBoutCompare]):
+        self._comparison.extend(compare)
+        return
+
+    def predict(self, matchup:SumoMatchup) -> {SumoWrestler, float}:
+        probability = 0.0
+        if not len(self._comparison):
+            # sys.stderr.write(f'No comparisons to make: randomly choosing\n')
+            probability = 2.0*random.random() - 1.0
+        else:
+            weight = 0.0
+            for c in self._comparison:
+                probability += c(matchup)
+                weight += c.weight()
+            probability = probability / weight
+
+        if probability >= 0.0:
+            # favor the Rikishi in an even match (exactly 0.0)
+            return matchup.rikishi, probability
+        else:
+            return matchup.opponent, probability
 
