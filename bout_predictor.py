265f1811ef06430dfaea15965ce598e9c9f7fcfc
Jeremy C. Andrus <jchristian.andrus@gmail.com>
2025-01-27 02:17:04 -0800

    Run predictions across all bouts in a tournament
    Signed-off-by: Jeremy C. Andrus <jchristian.andrus@gmail.com>

--- bout_predictor.py
+++ bout_predictor.py
@@ -11,82 +11,61 @@ import sys
 cdir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(cdir)
 
+from dataclasses import dataclass
+
 from sumostats.sumodata import *
 from sumostats.sumocalc import *
 
+# local sumo comparison functions!
 from compare_physical import *
 from compare_record import *
 
-sumodata = None
-try:
-    # save data to avoid lots of API queries
-    sumodata = SumoData.load_data('./sumo_data.pickle')
-except:
-    sumodata = SumoData()
 
-# load the 2025-01 basho data and fetch the latest info from the server
-basho = sumodata.get_basho('202501', SumoDivision.Makuuchi) #, fetch=True)
-if not basho:
-    basho = sumodata.get_basho('202501', SumoDivision.Makuuchi, fetch=True)
-banzuke = basho.get_banzuke(SumoDivision.Makuuchi)
-
-# save the data we just fetched
-sumodata.save_data('./sumo_data.pickle')
-#sumodata.print_table_stats()
-
-#
-# Create an empty prediction instance 
-#
-predictor = SumoBoutPredictor(sumodata)
-
-#
-# Create a list of comparison objects.
-# When constructing each, you can give a weight to that comparison.
-#
-physical_comparison:list[SumoBoutCompare] = [ \
-    CompareBMI(sumodata, 0.3), \
-    CompareHeight(sumodata, 0.2), \
-    CompareWeight(sumodata, 0.2), \
-    CompareRank(sumodata, 1.5), \
-    CompareBashoRecord(sumodata, 1.8), \
-    CompareHeadToHeadFull(sumodata, 0.8), \
-    CompareHeadToHeadCurrentDivision(sumodata, 1.7), \
-    CompareOverallRecord(sumodata, 1.2)
-]
-predictor.add_comparisons(physical_comparison)
+@dataclass()
+class BoutInfo():
+    match:BashoMatch
+    winner:SumoWrestler = None
+    projectedWinner:SumoWrestler = None
+    projectionConfidence:float = 0.0
 
 
+def GetProjectedWinners(sumodata, predictor, basho, division, day, boutlist, DEBUG=False) -> list[BoutInfo]:
+    bout_info = []
+    for bout in boutlist:
+        binfo = BoutInfo(bout)
 
-# get a list of upcoming bouts (the first day where no bout has a listed winner)
-# boutlist is a list of BashoMatch objects
-day = 0
-boutlist = []
-# day, boutlist = basho.get_upcoming_bouts(SumoDivision.Makuuchi)
+        if bout.winnerId > 0:
+            binfo.winner = sumodata.get_rikishi(bout.winnerId)
 
-# If there wasn't a list of upcoming bouts, then grab a random day and print it out
-if len(boutlist) == 0:
-    day = 9
-    boutlist = basho.get_bouts_in_division_on_day(SumoDivision.Makuuchi, day)
+        eastMatchup = sumodata.get_matchup(bout.eastId, bout.westId)
 
-print(f'\nBasho {basho.id_str()}, Day {day}')
+        # Run the prediction!
+        projectedWinner, confidence = predictor.predict(eastMatchup, basho, division, day, DEBUG=DEBUG)
 
-for bout in boutlist:
-    sys.stdout.write(f'{"-"*78}\nMatch: {bout.matchNo}\n')
+        binfo.projectedWinner = projectedWinner
+        binfo.projectionConfidence = confidence
 
-    east = sumodata.get_rikishi(bout.eastId)
-    eastRecord = banzuke.get_record_on_day(east.id(), day)
+        bout_info.append(binfo)
 
-    west = sumodata.get_rikishi(bout.westId)
-    westRecord = banzuke.get_record_on_day(west.id(), day)
+    return bout_info
+
+
+def PrintBoutInfo(sumodata, basho_match, eastId, eastRecord, westId, westRecord):
+    bashoId = basho_match.bashoId
+
+    #
+    # print header
+    #
+    sys.stdout.write(f'\n{"-"*79}\nMatch {basho_match.matchNo}\n')
+
+    east = sumodata.get_rikishi(eastId)
+    west = sumodata.get_rikishi(westId)
 
     #
     # Generate the matchup record for these two, use the "east" side as the
     # Rikishi in the matchup object. So it's "east" vs. "west" (opponent)
     #
     eastMatchup = sumodata.get_matchup(east.id(), west.id())
-
-    # Run the prediction!
-    projectedWinner, confidence = predictor.predict(eastMatchup, basho, SumoDivision.Makuuchi, day, DEBUG=False)
 
     #
     # Print Stats
@@ -113,12 +92,12 @@ for bout in boutlist:
         sys.stdout.write(f'{" "*35}')
 
     # Matchup History
-    if not eastMatchup or eastMatchup.total_matches() == 0:
-        sys.stdout.write(f'\nFirst meeting')
+    if not eastMatchup or eastMatchup.total_matches(beforeBasho=bashoId) == 0:
+        sys.stdout.write(f'\n    First meeting')
     else:
-        # last 6 basho
+        # last 6 basho (not including this one)
         nbasho = 6
-        for match in eastMatchup.each_match():
+        for match in eastMatchup.each_match(beforeBasho=bashoId):
             win_kimarite = ''
             lose_kimarite = ''
             if match.winnerId== east.id():
@@ -131,22 +110,105 @@ for bout in boutlist:
                 break
 
         # win[-fusensho] - loss[+fusenpai]
-        sys.stdout.write(f'\n    {eastMatchup.wins(True)}')
-        if eastMatchup.fusensho() > 0:
-            sys.stdout.write(f'[-{eastMatchup.fusensho()}]')
-        sys.stdout.write(f' - {eastMatchup.losses(True)}')
-        if eastMatchup.fusenpai() > 0:
-            sys.stdout.write(f'[+{eastMatchup.fusenpai()}]')
+        sys.stdout.write(f'\n    Head-to-Head Record\n    -------------------')
+        sys.stdout.write(f'\n    Overall: {eastMatchup.wins(True)}')
+        fusen=eastMatchup.fusensho(beforeBasho=bashoId)
+        if fusen > 0:
+            sys.stdout.write(f'[-{fusen}]')
+        sys.stdout.write(f' - {eastMatchup.losses(no_fusenpai=True, beforeBasho=bashoId)}')
+        fusen=eastMatchup.fusenpai(beforeBasho=bashoId)
+        if fusen > 0:
+            sys.stdout.write(f'[+{fusen}]')
 
         # win-loss in each division
         for div, matchup in eastMatchup.each_division():
             sys.stdout.write(f'\n    {div:10}: {matchup.rikishiWins}-{matchup.opponentWins}')
 
-    # Finall the good stuff: print the prediced winner!
-    sys.stdout.write(f'\nProjected Winner: {projectedWinner}, Confidence:{abs(confidence):.2%}')
-
-    if bout.winnerId > 0:
-        actualWinner = sumodata.get_rikishi(bout.winnerId)
-        sys.stdout.write(f'\nActual Winner: {actualWinner}')
-
     sys.stdout.write('\n\n')
+
+########################################################################
+#
+# Main
+#
+########################################################################
+
+# Load data
+sumodata = None
+try:
+    # save data to avoid lots of API queries
+    sumodata = SumoData.load_data('./sumo_data.pickle')
+except:
+    sumodata = SumoData()
+
+# Create an empty prediction instance 
+#
+predictor = SumoBoutPredictor(sumodata)
+
+#
+# Create a list of comparison objects.
+# When constructing each, you can give a weight to that comparison.
+#
+comparisons:list[SumoBoutCompare] = [ \
+    CompareBMI(sumodata, 0.3), \
+    CompareHeight(sumodata, 0.2), \
+    CompareWeight(sumodata, 0.2), \
+    CompareRank(sumodata, 1.3), \
+    CompareBashoRecord(sumodata, 1.8), \
+    CompareHeadToHeadFull(sumodata, 0.8), \
+    CompareHeadToHeadCurrentDivision(sumodata, 1.7), \
+    CompareOverallRecord(sumodata, 1.2)
+]
+predictor.add_comparisons(comparisons)
+
+
+# load the 2025-01 basho data and fetch the latest info from the server
+basho = sumodata.get_basho('202501', SumoDivision.Makuuchi)
+if not basho:
+    basho = sumodata.get_basho('202501', SumoDivision.Makuuchi, fetch=True)
+
+# save the data we just fetched
+sumodata.save_data('./sumo_data.pickle')
+#sumodata.print_table_stats()
+
+
+bout_info_on_day:dict[int, list[BoutInfo]] = {}
+
+division = SumoDivision.Makuuchi
+banzuke = basho.get_banzuke(division)
+
+num_days = basho.num_days()
+for day in range(1, num_days+1):
+    boutlist = basho.get_bouts_in_division_on_day(division, day)
+    bout_info_on_day[day] = GetProjectedWinners(sumodata, predictor, basho, division, day, boutlist, DEBUG=False)
+
+
+correctPredictions = 0
+totalPredictions = 0
+
+# Print it all out
+for day in range(1, num_days+1):
+    print(f'\n{"*"*79}\nBasho {basho.id_str()}, Day {day}, {division}')
+    for bout in bout_info_on_day[day]:
+        east = sumodata.get_rikishi(bout.match.eastId)
+        eastRecord = banzuke.get_record_on_day(east.id(), day)
+        west = sumodata.get_rikishi(bout.match.westId)
+        westRecord = banzuke.get_record_on_day(west.id(), day)
+
+        sys.stdout.write(f'\nMatch {bout.match.matchNo}\n')
+        sys.stdout.write(f'    {east} vs. {west}\n')
+        # PrintBoutInfo(sumodata, bout.match, eastId, eastRecord, westId, westRecord)
+
+        # After the bout info, print out the projected winner and actual winner
+        sys.stdout.write(f'        Projected Winner: {bout.projectedWinner}, Confidence:{abs(bout.projectionConfidence):.2%}\n')
+        if bout.winner:
+            sys.stdout.write(f'        Actual Winner: {bout.winner}\n')
+            if bout.projectedWinner.id() == bout.winner.id():
+                correctPredictions += 1
+        totalPredictions += 1
+
+sys.stdout.write('\n\n')
+
+pct = float(correctPredictions) / float(totalPredictions)
+print(f'Total Predictions: {totalPredictions}')
+print(f'Correct Predictions: {correctPredictions} ({pct:.2%})')
+
